82a412b911ea8ffd8687a45bfb682b0f5d2f9c4b
jpmarcos <jpmarcos86@outlook.com>
2016-05-12 13:42:19 +0200

    Little fix

--- public/javascripts/controllers/gestor.js
+++ public/javascripts/controllers/gestor.js
@@ -1,64 +1,52 @@
 var app = angular.module('myApp', []);
 
 app.controller('gestor', function($scope, $http) {
-
   $scope.servidores = [];
-  
 
-  $scope.guardarServidor = function(servidor){
-    var tmp = {
-      servidor: servidor,
-      procesos: []
-    };
+  $scope.guardarServidor = function(servidor) {
     $http({
-      method:'GET',
-      url: "http://"+ tmp.servidor +"/servicio/informacion?self=true",
+      method: 'GET',
+      url: "http://" + servidor + "/servicio/informacion?self=true",
     }).then(function successCallback(response) {
       console.log(response.data);
-      response.data.procesos.forEach(function(value){
-        tmp.procesos.push({
-          id: value
-        });
+      $scope.servidores.push({
+        servidor: servidor,
+        procesos: response.data.procesos
       });
-      $scope.servidores.push(tmp);
-    }, function errorCallback(response){
-    });
-
+    }, function errorCallback(response) {});
   };
 
-  $scope.crear = function(pro, servidor){
-    servidor.procesos.push({id:pro});
+  $scope.crear = function(pro, servidor) {
+    servidor.procesos.push(pro);
     $http({
-      method:'GET',
-      url: "http://"+ servidor.servidor +"/servicio/informacion",
+      method: 'GET',
+      url: "http://" + servidor.servidor + "/servicio/informacion",
     }).then(function successCallback(response) {
       console.log(response.data);
-    }, function errorCallback(response){
-    });
+    }, function errorCallback(response) {});
   }
 
-  $scope.arrancar = function(pro, servidor){
-    
+  $scope.arrancar = function(pro, servidor) {
     $http({
-      method:'GET',
-      url: "http://"+ servidor +"/servicio/arrancar",
-      params: {id: pro},
+      method: 'GET',
+      url: "http://" + servidor + "/servicio/arrancar",
+      params: {
+        id: pro
+      },
     }).then(function successCallback(response) {
       console.log(response.data);
-    }, function errorCallback(response){
-    });
-
+    }, function errorCallback(response) {});
   };
 
-  $scope.parar = function(pro, servidor){
+  $scope.parar = function(pro, servidor) {
     $http({
-      method:'GET',
-      url: "http://"+ servidor +"/servicio/parar",
-      params: {id: pro},
+      method: 'GET',
+      url: "http://" + servidor + "/servicio/parar",
+      params: {
+        id: pro
+      },
     }).then(function successCallback(response) {
       console.log(response.data);
-    }, function errorCallback(response){
-    });
-
+    }, function errorCallback(response) {});
   };
 });
