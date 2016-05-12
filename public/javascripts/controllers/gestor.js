00602ac711373fe2b132cb0edf1bfdf973162d16
Emilio José <choco@usal.es>
2016-05-12 14:10:02 +0200

    fix

--- public/javascripts/controllers/gestor.js
+++ public/javascripts/controllers/gestor.js
@@ -2,17 +2,24 @@ var app = angular.module('myApp', []);
 
 app.controller('gestor', function($scope, $http) {
   $scope.servidores = [];
+  var info = {};
 
   $scope.guardarServidor = function(servidor) {
     $http({
       method: 'GET',
-      url: "http://" + servidor + "/servicio/informacion?self=true",
+      url: "http://" + servidor + "/servicio/informacion",
+      params: {
+        self: true
+      },
     }).then(function successCallback(response) {
       console.log(response.data);
       $scope.servidores.push({
         servidor: servidor,
         procesos: response.data.procesos
       });
+      response.data.procesos.foreach(function(value{
+        info{value} = infoProcesos(value, servidor);
+      }))
     }, function errorCallback(response) {});
   };
 
@@ -49,4 +56,17 @@ app.controller('gestor', function($scope, $http) {
       console.log(response.data);
     }, function errorCallback(response) {});
   };
+
+  var infoProcesos = function(pro, servidor) {
+    $http({
+      method: 'GET',
+      url: "http://" + servidor + "/servicio/informacion",
+      params: {
+        id: pro
+      },
+    }).then(function successCallback(response) {
+      console.log(response.data);
+      return response.data;
+    }, function errorCallback(response) {});
+  }
 });
