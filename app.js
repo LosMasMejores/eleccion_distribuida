83661bd1792f85c22e2457c2e69a33ad52b5bc3d
Javier Pérez Marcos <Javier Prez Marcos>
2016-05-13 10:00:40 +0200

    Little fix

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', (req, res) => {
+  res.redirect('/gestor');
+})
 app.use('/servicio', servicio);
 app.use('/gestor', gestor);
 
